490c43984c2b66e5dd838722e5a760fcb31809db
AlePuccini-uk <210629170+AlePuccini-uk@users.noreply.github.com>
2025-06-11 13:29:29 +0100

    auto-commit: Update app.py with new feature

--- app.py
+++ app.py
@@ -3,22 +3,29 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles.csv')  # lendo os dados
-hist_button = st.button('Criar histograma')  # criar um botão
 
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
+
+st.header('ANÁLISE DE VENDAS DE CARROS')  # título do aplicativo
+
+
+# Criar botões
+hist_button = st.button('Criar histograma')
+scatter_button = st.button('Criar gráfico de dispersão')
+
+# Se o botão de histograma for clicado
+if hist_button:
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig_hist = px.histogram(car_data, x="odometer")
 
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_hist, use_container_width=True)
 
-    # criar uma caixa de seleção
-build_histogram = st.checkbox('Criar um histograma')
+# Se o botão de gráfico de dispersão for clicado
+if scatter_button:
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
-if build_histogram:  # se a caixa de seleção for selecionada
-    st.write('Criando um histograma para a coluna odometer')
-    ...
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig_scatter, use_container_width=True)
